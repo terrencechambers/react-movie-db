f788166a9253b4ae225164b4c617b9e6ea4b82fe
Terrence <tj.chambers@gmail.com>
2021-08-24 18:40:29 -0500

    Added thumb styles

--- src/components/Thumb/Thumb.styles.js
+++ src/components/Thumb/Thumb.styles.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export const Image = styled.img`
-    
+    width: 100%;
+    max-width: 45rem;
+    transition: all 0.3s;
+    object-fit: cover;
+    border-radius: 20px;
+    animation: animateThumb 0.5s;
+
+    :hover {
+        opactiy: 0.8;
+    }
+
+    @keyframes animatethumb {
+        from {
+            opacity: 0;
+        }
+        to {
+            opactiy: 1;
+        }
+    }
 `;
