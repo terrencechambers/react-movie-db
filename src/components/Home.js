68a07de34274a2ae0956d1f5d933f32455273d57
Terrence <tj.chambers@gmail.com>
2021-08-24 18:50:34 -0500

    Created spinner, removed import from home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
@@ -7,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -40,6 +39,7 @@ const Home = () => {
                     />
                 ))}
             </Grid>
+            <Spinner />
         </>
     )
 };
