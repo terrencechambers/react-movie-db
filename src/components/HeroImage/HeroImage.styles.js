9bbf6b9e126cfc5e382a3febb08a0704440599dc
Terrence <tj.chambers@gmail.com>
2021-08-23 19:51:28 -0500

    Added Hero Image styling

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -1,8 +1,62 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    background: linear-gradient(
+        to bottom, rgba(0, 0, 0, 0)
+        41%, rgba(0, 0, 0, 0.65)
+        100%
+    ),
+        url(${props => props.image}), var(--darkGrey);
+    background-size: 100%, cover;
+    background-position: center;
+    height: 40rem;
+    position: relative;
+    animation: animateHeroImage 1s;
 
-export const Content = styled.div``;
+    @keyframes animateHeroImage {
+        from {
+            opacity: 0;
 
-export const Text = styled.div``;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+`;
+
+export const Content = styled.div`
+    padding: 1.5rem;
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+`;
+
+export const Text = styled.div`
+    z-index: 100;
+    max-width: 45rem;
+    position: absolute;
+    bottom: 2rem;
+    margin-right: 1.5rem;
+    min-height: 12rem;
+    color: var(--white);
+
+    h1 {
+        font-size: var(--fontSuperBig);
+
+        @media screen and (max-width: 720px) {
+            font-size: var(--fontBig);
+        }
+    }
+
+    p {
+        font-size: var(--fontMed);
+
+        @media screen and (max-width: 720px) {
+            font-size: var(--fontSmall);
+        }
+    }
+
+    @media screen and (max-width: 720px) {
+        max-width: 100%;
+    }
+`;
 
