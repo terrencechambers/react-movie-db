36e964d2ee78c08ef473e08c05c171e4ed764b5b
Terrence <tj.chambers@gmail.com>
2021-08-23 19:33:55 -0500

    Added Hero Component
    Added hero component styles and js, added to home js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // Components
-
+import HeroImage from './HeroImage';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -14,9 +14,20 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
     const { state, loading, error } = useHomeFetch();
-    
+
     console.log(state);
-    return <div>Home Page</div>
+    return (
+        <>
+            {state.results[0] ?
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+            : null
+            }
+        </>
+    )
 };
 
 export default Home;
