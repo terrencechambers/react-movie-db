222a97045a7be602267f9503875c0cd0cb5ac760
Terrence <tj.chambers@gmail.com>
2021-08-26 19:02:55 -0500

    Added button component
    Added logic to show button to load or loader if loading

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+// Styles
+import { Wrapper } from "./Button.styles";
+
+const Button = ({ text, callback }) => (
+    <Wrapper type="button" onclick={callback}>
+        {text}
+    </Wrapper>
+)
+
+export default Button;
