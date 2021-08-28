79d78c7540cff6be9c2d7e035c3ae51c9c238039
Terrence <tj.chambers@gmail.com>
2021-08-27 23:40:57 -0500

    Added Link to head and thumbnail

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
@@ -6,7 +8,9 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 const Header = () => (
     <Wrapper>
         <Content>
-            <LogoImg src={RMDBLogo} alt='RMDB logo' />
+            <Link to='/'>
+                <LogoImg src={RMDBLogo} alt='RMDB logo' />
+            </Link>
             <TMDBLogoImg src={TMDBLogo} alt="TMDB logo" />
         </Content>
     </Wrapper>
