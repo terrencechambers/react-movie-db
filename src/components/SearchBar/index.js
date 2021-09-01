1b6b1fb74f9c532d932396567228b3498cc8a171
Terrence <tj.chambers@gmail.com>
2021-08-31 21:59:28 -0500

    Added class function in comments on search bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,6 @@
 import { useState, useEffect, useRef } from "react";
+// import { Component } from "react"; This is for class functions
+
 import PropTypes from 'prop-types';
 
 // Image
@@ -7,16 +9,53 @@ import searchIcon from '../../images/search-icon.svg';
 //Styles
 import { Wrapper, Content } from "./SearchBar.style";
 
+// class SearchBar extends Component { this is how to set a class function
+
+// functional component
 const SearchBar = ({ setSearchTerm }) => {
+    // This replaces the state and initial variables when using class function
+    // state = { value: '' };
+    // timeout = null;
+
+    // componentDidUpdate(_prevProps, prevState) {
+    //      if (this.state.value !== prevState.value) {
+    //          const {setSearchTerm } = this.props;
+    //          clearTimeout(this.timeout);
+    //            this.timeout = setTimeout(() => {
+    //                const {value } = this.state;
+    //                setSearchTerm(value);
+    //            }, 500);
+    //      }
+    //}
+
+    // render() {
+    //     return (
+    //         <Wrapper>
+    //             <Content>
+    //                 <img src={searchIcon} alt='search icon' />
+    //                 <input
+    //                     type='text'
+    //                     placeholder='Search Movie'
+    //                     onChange={event => this.setState({ value: event.currentTarget.value})
+    //                     value={this.state.value}
+    //                 />
+    //             </Content>
+    //         </Wrapper>
+    //     );
+    // }
+
+    // Not used with class function
     const [state, setState] = useState('');
     const initial = useRef(true);
 
+    // useEffect is not used with class component
     useEffect(() => {
         if (initial.current) {
             initial.current = false;
             return;
         }
 
+        // for class moved this inside componentDidUpdate
         const timer = setTimeout(() => {
             setSearchTerm(state);
         }, 500);
@@ -24,6 +63,7 @@ const SearchBar = ({ setSearchTerm }) => {
         return () => clearTimeout(timer);
     },[setSearchTerm, state]);
 
+    // this is moved into the render() with class function
     return (
         <Wrapper>
             <Content>
